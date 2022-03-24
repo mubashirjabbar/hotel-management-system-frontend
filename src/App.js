613a435e6915f8f6281aced893ca6c58c7f533d2
mubashir <mubashir.jabbar97@gmail.com>
2022-03-24 17:08:38 +0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+import Home from "./screens/home/Home";
+import Login from "./screens/login/Login";
+import Signup from "./screens/signup/Signup";
+import Pagenotfound from "./screens/pagenotfound/Pagenotfound";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/signup" exact component={Signup} />
+            <Route component={Pagenotfound} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
